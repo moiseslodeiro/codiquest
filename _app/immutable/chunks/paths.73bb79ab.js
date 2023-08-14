153b36b3108a37812fa2af2b66e4359530e87793
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-08-14 14:49:19 +0100

    Updates

--- _app/immutable/chunks/paths.73bb79ab.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_31h0m2)==null?void 0:s.base)??"/codiquest";var e;const a=((e=globalThis.__sveltekit_31h0m2)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/nodes/4.f85f782a.js
@@ -0,0 +1,1 @@
+import{s as o,n as s}from"../chunks/scheduler.e108d1fd.js";import{S as r,i,m as c,n as m,a as l,f as p}from"../chunks/index.0719bd3d.js";function f(a){let t;return{c(){t=c("Test")},l(e){t=m(e,"Test")},m(e,n){l(e,t,n)},p:s,i:s,o:s,d(e){e&&p(t)}}}class _ extends r{constructor(t){super(),i(this,t,null,f,o,{})}}export{_ as component};
