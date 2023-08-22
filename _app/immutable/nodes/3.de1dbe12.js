5cf7403a7ea0fa6c09541576f34f59dce9df5fbd
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-08-22 10:25:17 +0100

    Updates

--- _app/immutable/nodes/3.de1dbe12.js
+++
@@ -1,1 +0,0 @@
-import{s as n,l as r,m as i,i as l,y as a,d as c}from"../chunks/scheduler.fb34ef27.js";import{S as m,i as p}from"../chunks/index.81d24910.js";function f(s){let e;return{c(){e=r("Level page!")},l(t){e=i(t,"Level page!")},m(t,o){l(t,e,o)},p:a,i:a,o:a,d(t){t&&c(e)}}}class x extends m{constructor(e){super(),p(this,e,null,f,n,{})}}export{x as component};
