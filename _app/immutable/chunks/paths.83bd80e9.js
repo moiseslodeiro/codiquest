8568f94515a867c781e012152103d1d96940c864
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-08-22 11:58:00 +0100

    Updates

---
+++ _app/immutable/chunks/paths.83bd80e9.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1sl1cc4)==null?void 0:s.base)??"/codiquest";var e;const a=((e=globalThis.__sveltekit_1sl1cc4)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/nodes/6.4726f812.js
@@ -0,0 +1,1 @@
+import{P as m}from"../chunks/6.b4158a88.js";export{m as component};
