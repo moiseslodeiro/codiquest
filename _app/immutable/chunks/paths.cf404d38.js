69c40461bef445551ccebacdbd2e9302c02c9ede
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-10-24 22:07:45 +0100

    Updates

---
+++ _app/immutable/chunks/paths.cf404d38.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1q53ws5)==null?void 0:s.base)??"";var e;(e=globalThis.__sveltekit_1q53ws5)==null||e.assets;export{t as b};
--- _app/immutable/chunks/routes.263bd043.js
+++
@@ -1,1 +0,0 @@
-const e=["/","/admin","/about","/404","/js/level/6","/js","/js/level","/js/level/5","/js/level/4","/js/level/3","/js/level/2","/js/level/1","/js/level/0","/git/level/0","/git","/git/level","/codiquest"];export{e as r};
