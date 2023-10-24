211a5863ad913d405c4a0ca9f5b2b1b2a91df7aa
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-10-25 00:13:57 +0100

    Updates

--- _app/immutable/chunks/paths.c6d27a13.js
+++
@@ -1,1 +0,0 @@
-var s;const l=((s=globalThis.__sveltekit_16nlehp)==null?void 0:s.base)??"";var e;(e=globalThis.__sveltekit_16nlehp)==null||e.assets;export{l as b};
---
+++ _app/immutable/chunks/paths.cd362768.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_pwqewj)==null?void 0:s.base)??"";var e;(e=globalThis.__sveltekit_pwqewj)==null||e.assets;export{t as b};
