60229e424b4ed5d47705a3548bb6891bf51a3c5a
Moisés Lodeiro <moises.lodeiro@gmail.com>
2025-09-18 11:06:28 +0100

    Replacing npm for pnpm in workflow

---
+++ src/routes/about/+page.js
@@ -0,0 +1,6 @@
+// CodiQuest
+// To prevent mocks from appearing in the final version, when building,
+// it is set the prerenter const to false and add the "strict: false"
+// to the adapter in svelte.config.js
+
+export const prerender = false;
